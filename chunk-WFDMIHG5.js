6ce87f57211bd16c1d25130979915b7718618478
Mads Holten Rasmussen <mads.h.rasmussen@gmail.com>
2024-12-03 21:04:22 +0100

    Added model viewer

--- chunk-WFDMIHG5.js
+++
@@ -1,1 +0,0 @@
-import{Mb as s,_a as t,tb as n,ub as r}from"./chunk-ZKKSPBLY.js";var c=class o{static \u0275fac=function(e){return new(e||o)};static \u0275cmp=t({type:o,selectors:[["app-resource"]],decls:2,vars:0,template:function(e,a){e&1&&(n(0,"p"),s(1,"resource works!"),r())},encapsulation:2})};export{c as ResourceComponent};
